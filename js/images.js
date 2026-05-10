0a082a8d7b0e20ae650bf0e585de8b8979eff724
tree8s <166245068+tree8s@users.noreply.github.com>
2026-05-10 22:00:22 +0200

    pushing in case something goes wrong

--- js/images.js
+++ js/images.js
@@ -1,12 +1,14 @@
-const items = document.querySelectorAll(".timeline ul li");
-const images = document.querySelectorAll(".view-img");
+items = document.querySelectorAll(".timeline ul li");
+images = document.querySelectorAll(".view-img");
 
 items.forEach(item => {
     item.addEventListener("mouseenter", () => {
-        const targetId = item.dataset.target;
+        targetId = item.dataset.target;
 
         images.forEach(img => img.classList.remove("active"));
-
         document.getElementById(targetId).classList.add("active");
+
+        items.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
     });
 });
